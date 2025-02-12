15676b2ec6515025c23d65123cbaaa10cd493b63
Simon Dahlberg <simondhlbrg@gmail.com>
2025-02-12 18:31:51 +0200

    Enable fullscreen on display

--- apps/timer-web/src/routes/[timerId]/display/+page.svelte
+++ apps/timer-web/src/routes/[timerId]/display/+page.svelte
@@ -3,9 +3,26 @@
 	import { getContext } from 'svelte';
 
 	const service = getContext('service') as TimerService;
+	let container: HTMLDivElement;
+	let fullscreenState: 'windowed' | 'fullscreen' = 'windowed';
+
+	function toggleFullscreen() {
+		if (fullscreenState === 'windowed') {
+			container.requestFullscreen().then(() => {
+				fullscreenState = 'fullscreen';
+			});
+		} else {
+			document.exitFullscreen();
+			fullscreenState = 'windowed';
+		}
+	}
 </script>
 
-<div class="flex h-screen items-center justify-center p-5">
+<div
+	bind:this={container}
+	class="flex h-screen items-center justify-center bg-white p-5"
+	onpointerup={toggleFullscreen}
+>
 	<svg viewBox="0 0 70 20">
 		<text x="1" y="12">
 			{service.formattedTime}
